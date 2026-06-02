a6b04c02fbdbea6c9f258dcb348d65b85c5044f0
Denis <d.steph.dev@gmail.com>
2026-06-02 21:11:48 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+
+namespace TodoApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            app.MapGet("/", () => $"Hello world!");
+
+            app.Run();
+        }
+    }
+}
