c962dd105038d231646288e653157a24aac705c8
Denis <d.steph.dev@gmail.com>
2026-06-02 21:48:35 +0400

    Models created

---
+++ Model/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TodoApi.Model
+{
+    public class TaskModel
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateComplitionExpected { get; set; }
+        public List<UserImplementor>? ImplementorsList { get; set; } = [];
+        public DateTime? DateUpdated { get; set; }
+        public decimal? CurrentStatus { get; set; }
+
+    }
+}
---
+++ Model/UserImplementor.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Model
+{
+    public class UserImplementor
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+    }
+}
