d11aacef667d30e3bafa019f27db30f17081fa80
Denis <d.steph.dev@gmail.com>
2026-06-08 19:12:04 +0400

    feat: add Serilog so I can log data into file!

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using TodoApi.DataAccess;
 using TodoApi.DataAccess.Repositories;
 using TodoApi.Middleware;
 using TodoApi.Services;
+using Serilog;
 
 namespace TodoApi
 {
@@ -10,30 +11,50 @@ namespace TodoApi
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContext") ?? throw new InvalidOperationException("Connection string ApplicationDBContext not found!");
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build())
+                .CreateLogger();
 
-            builder.Services.AddControllers();
-            builder.Services.AddSwaggerGen(o =>
+            try
             {
-                o.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo { Title = "astroToDo.api", Version = "v1" });
-            });
-            builder.Services.AddDbContext<TodoDbContext>(o => o.UseSqlServer(connectionString));
-            builder.Services.AddScoped<ITodoTaskRepository, TodoTaskRepository>();
-            builder.Services.AddScoped<ITodoTasksService, TodoTasksService>();
+                Log.Information("Application is starting...");
 
-            var app = builder.Build();
+                var builder = WebApplication.CreateBuilder(args);
+                var connectionString = builder.Configuration.GetConnectionString("ApplicationDBContext") ?? throw new InvalidOperationException("Connection string ApplicationDBContext not found!");
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(o => { o.SwaggerEndpoint("/swagger/v1/swagger.json", "astroToDo api v1"); });
+                builder.Host.UseSerilog();
+                builder.Services.AddControllers();
+                builder.Services.AddSwaggerGen(o =>
+                {
+                    o.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo { Title = "astroToDo.api", Version = "v1" });
+                });
+                builder.Services.AddDbContext<TodoDbContext>(o => o.UseSqlServer(connectionString));
+                builder.Services.AddScoped<ITodoTaskRepository, TodoTaskRepository>();
+                builder.Services.AddScoped<ITodoTasksService, TodoTasksService>();
+
+                var app = builder.Build();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI(o => { o.SwaggerEndpoint("/swagger/v1/swagger.json", "astroToDo api v1"); });
+                }
+
+                app.UseMiddleware<RequestLoggingMiddleware>();
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseMiddleware<RequestLoggingMiddleware>();
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application was shut down with fatal error on start!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
